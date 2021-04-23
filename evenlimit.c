40cd30ee971f175eeef861b21c50f98708ace43e
devadathank <82220641+devadathank@users.noreply.github.com>
2021-04-23 15:34:11 +0530

    Add files via upload

---
+++ evenlimit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int n=1,count=0;
+  while(count<20)
+   {
+     if(n%2==0)
+     {
+       printf("%d",n);
+       count=count+1;
+     }
+     n=n+1;
+    }
+
+}
