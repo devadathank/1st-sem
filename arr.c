189d93289baf227b9d639c962809bcf0bcdbd0a1
devadathank <82220641+devadathank@users.noreply.github.com>
2021-04-26 11:00:08 +0530

    Add files via upload

---
+++ arr.c
@@ -0,0 +1,22 @@
+include<stdio.h>
+void main()
+{
+  int n,a[100],sum=0,i;
+  printf("Enter the number (less than or equal to 100)");
+  scanf("%d",&n);
+    if(n<=100)
+    {
+      printf("Enter the %d numbers: ",n);
+      for(i>0;i<n;i++)
+      {
+      scanf("%d",&a[i]);
+      sum=sum+a[i];
+      }
+      printf("sum = %d ",sum);
+    }
+
+    else
+    {
+      printf("Please enter a number less than or equal to 100");
+    }
+}
